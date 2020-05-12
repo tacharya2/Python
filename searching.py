da31d33a83158a9d0500f24cfb6468eaabc247b8
tacharya2 <60335154+tacharya2@users.noreply.github.com>
2020-05-12 12:42:10 -0400

    line added

--- searching.py
+++ searching.py
@@ -1,8 +1,10 @@
-#search for 9
+#search for n and it's position
+pos = -1
 def search(arr,n):
     i = 0
     while i<len(arr):
         if arr[i] == n:
+            globals()['pos'] = i
             return True
         i = i+1
     return False
@@ -10,6 +12,6 @@ def search(arr,n):
 arr = [5, 8, 4, 6, 9, 2]
 n = int(input("enter a number to check: "))
 if search(arr,n):
-    print("It exists")
+    print("It exists at",pos)
 else:
     print("It does not exists")
