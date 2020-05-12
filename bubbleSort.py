6e76e345ac6abf0da129689a7216c5702976c34a
tacharya2 <60335154+tacharya2@users.noreply.github.com>
2020-05-12 17:15:30 -0400

    line added

--- bubbleSort.py
+++ bubbleSort.py
@@ -1,3 +1,11 @@
+def bubbleSort(nums):
+    for i in range(len(nums)-1, 0 , -1):
+        for j in range(i):
+            if nums[j]>nums[j+1]:
+                # j is index, [j] is jth value
+                temp = nums[j]
+                nums[j] = nums[j+1]
+                nums[j+1] = temp
 nums = [5, 3, 8, 6, 7, 2]
-
 bubbleSort(nums)
+print(nums)
