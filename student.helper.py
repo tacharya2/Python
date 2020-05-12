408964e35682c58c99f7d4b13defa567ec9da35e
tacharya2 <60335154+tacharya2@users.noreply.github.com>
2020-05-12 10:35:46 -0400

    line added

--- student.helper.py
+++ student.helper.py
@@ -1,4 +1,4 @@
-print("Use this to connect degree into Farenheit")
+print("Use this to convert degree temp into Farenheit scale")
 d = float(input("please enter degree temperature: "))
 y = (9/5 * d) + 32
 print(y)
