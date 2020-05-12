b54bafea2917d061ef5e95396e6480652e56eea7
tacharya2 <60335154+tacharya2@users.noreply.github.com>
2020-05-12 16:55:19 -0400

    line added

--- BinarySearch.py
+++ BinarySearch.py
@@ -13,10 +13,11 @@ def search(array,n):
             else:
                 u = mid-1
     return False
-array = [123, 197, 236, 391, 456, 5321, 684, 777, 836, 925, 147, 987]
+array = [123, 197, 236, 391, 456, 532, 684, 777, 836, 925, 1147, 1987]
 # The data is unsorted. How can we sort and use it for binary search?
+#array = array1.sort()
 n = int(input("Enter a number to search: "))
 if search(array, n):
-    print("The number exists at ", pos+1, "position")
+    print("The number exists at position ", pos+1)
 else:
     print("the search number does not exists")
---
+++ bubbleSort.py
@@ -0,0 +1,3 @@
+nums = [5, 3, 8, 6, 7, 2]
+
+bubbleSort(nums)
