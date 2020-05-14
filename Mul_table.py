c17772255137b252f4bb52b9e17bd3175d24f0b8
tacharya2 <60335154+tacharya2@users.noreply.github.com>
2020-05-14 10:42:04 -0400

    Update Mul_table.py

--- Mul_table.py
+++ Mul_table.py
@@ -1,5 +1,5 @@
-m = int(input("Multiplication table of what number n ?: "))
-n = int(input("Upto what range m ?: "))
+m = int(input("Multiplication table of what number m ?: "))
+n = int(input("Upto what range n ?: "))
 for i in (1, n):
     for j in range(n+1):
         print(m," * ", j, " = ", m * j)
