15a905043995d1bd429f9333b6ed2bb93bc0f142
tacharya2 <60335154+tacharya2@users.noreply.github.com>
2020-05-14 16:20:44 -0400

    Add files via upload

--- Armstrong_number.py
+++ Armstrong_number.py
@@ -1,11 +1,12 @@
 # if 123 = 1^3+2^3+3^3, then 123 is an Armstrong number
 # Check if a given 3 digit number is an Armstrong number
 num = int(input("Enter a number: "))
+order = int(input("What's the order of the number?: "))
 sum = 0
 temp = num
 while temp>0:
     digit = temp % 10 # digit = 3, 5, 1^3
-    sum += digit**3 # sum = 3^3+5^3+1^3
+    sum += digit**order # sum = 3^3+5^3+1^3
     temp //= 10 # temp = 15, 1, 0<-terminates
 if num == sum:
     print(num, " is an Armstrong number")
