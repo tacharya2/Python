60ee8e93e6c7d4ccbb4678ea458873ec68b78783
tacharya2 <60335154+tacharya2@users.noreply.github.com>
2020-05-14 23:04:46 -0400

    Add files via upload

---
+++ ASCII.py
@@ -0,0 +1,11 @@
+# make a table of ASCII values of lower-case alphabet
+# fileU.txt = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+# fileL.txt = 'abcdefghijklmnopqrstuvwxyz'
+file = open("fileL.txt")
+while 1:
+    char = file.read(1)
+    if not char:
+        break
+    for i in char:
+        print(i,":\t",ord(char))
+
