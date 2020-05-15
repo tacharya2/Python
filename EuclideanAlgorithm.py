b2381bad8bd97dc5f18ddfd8eb55aad60b7d725d
tacharya2 <60335154+tacharya2@users.noreply.github.com>
2020-05-15 10:13:15 -0400

    Add files via upload

---
+++ EuclideanAlgorithm.py
@@ -0,0 +1,8 @@
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+def compute_hcf(x,y):
+    while(y): # greater
+        x,y = y,x % y
+    return x
+hcf = compute_hcf(num1,num2)
+print(hcf)
